8b004bf5d09cc2a10d9f3342a8dbca0cfe5b8cb4
Andy Willis <awillis@fastmail.fm>
2023-03-23 10:24:23 +0000

    First commit

---
+++ batchrename.py
@@ -0,0 +1,44 @@
+import os, glob, uuid
+
+from pathlib import Path
+
+from file import getListFromFileContents
+
+# init
+csvFile = 'input\\fileList3.csv'
+no = 1
+
+csvList = getListFromFileContents(csvFile)
+
+# For each line in the cvs list
+for line in csvList:
+
+  # Split the line by a comma into a list
+	item = line.split(',')
+
+	# The path is the first two elements
+	path = ''.join(item[0:1])
+
+	# The filename is the first element which
+	# we split into its prefix and extension
+	filename = item[1].split('.')
+
+	# Remake the prefix by joining the filename with periods
+	prefix = '.'.join(filename[0:-1])
+	
+	# Because there maybe other periods in the
+	# filename the extension will always be the last one
+	ext = filename[-1]
+	
+	# New hex uuid
+	hex = uuid.uuid4().hex
+
+	# Set the new paths
+	previous = f'{path}{prefix}.{ext}'
+	next = f'{path}{hex}_{no}.{ext}'
+	
+	# print(f'{previous}...{next}')
+	
+	os.rename(previous, next)
+	
+	no = no + 1
---
+++ file.py
@@ -0,0 +1,37 @@
+import os
+
+# `createFile`
+#
+# Create a new file
+def createFile(filePath):
+
+    with open(filePath, 'a+') as myfile:
+        myfile.close()
+
+
+# `getListFromFileContents`
+#
+# Create an empty file if it doesn't exist, and return
+# an empty array, or read the lines of the file if it does
+# exist, and return a filled array
+def getListFromFileContents(filePath):
+
+    list = []
+
+    if os.path.isfile(filePath) is False:
+
+        createFile(filePath)
+
+    else:
+
+        file = open(filePath, 'r')
+        line = file.readline()
+
+        while line:
+
+            list.append(line.rstrip('\n'))
+            line = file.readline()
+
+        file.close()
+
+    return list
